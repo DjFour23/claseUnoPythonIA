0c47efecec95039650f356b7f1aecd3df08e539e
Diego Benavides <diegojose2322@hotmail.com>
2022-02-23 20:11:44 -0500

    Primer commit

---
+++ ClaseUnoPython.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# Variables
+
+a= 3
+print(type(a))
+a= "hola"
+print(type(a))
+a =3.5
+print(type(a))
+a= True
+print(type(a))
+
+print('Hola Mundo')
+print('Hola Mundo dos')
+
+print("I can't do it")
+print('Su nombre es "Diego"')
+
+#Operaciones en esta wea
+
+#suma
+a=5
+b=2
+c= a + b
+print(c)
+
+#resta
+a=5
+b=2
+c= a - b
+print(c)
+
+#Multiplicacion
+a=5
+b=2
+c= a * b
+print(c)
+
+#Division
+a=5
+b=2
+c= a / b
+print(c)
+
+#Division parte entera
+a=5
+b=2
+c= a // b
+print(c)
+
+#Potencia
+a=2
+b=6
+c= a ** b
+print(c)
+
+#Raiz cubica
+a=16
+b=a ** (1/3)
+print(b)
+
+#Raiz cuadrada
+import math
+raiz = math.sqrt(a)
+print(raiz)
+
+#Tipos de datos
+
+# String str
+
+a = "Hola Mundo"
+a = 'Hola Mundo'
+a = "I can't do it"
+a ='Su nombre es "Diego"'
+
+
+
+
+
